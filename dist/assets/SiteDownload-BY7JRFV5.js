9e4b807fb415b04fbc3e0a15653e4042c6994b07
Akbar <akbarkarimov250@gmail.com>
2024-04-11 23:39:17 +0500

    Akbar

---
+++ dist/assets/SiteDownload-BY7JRFV5.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,b as c}from"./index-D-sRmK2v.js";const s={},n=c("h1",null," test ",-1),_=[n];function a(r,l){return o(),t("div",null,_)}const i=e(s,[["render",a]]);export{i as default};
