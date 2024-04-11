3691b7d4fbdd8719ba2cd75b9bbd61b4fb72a4a1
Akbar <akbarkarimov250@gmail.com>
2024-04-12 01:22:09 +0500

    Akbar

---
+++ client/src/layouts/LandingLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="site">
+    <div class="container">
+      <Navbar />
+      <main class="main">
+        <router-view />
+      </main>
+      <Footer />
+    </div>
+    <div class="site-bg"></div>
+  </section>
+</template>
+
+<style lang="scss">
+  @import '@/assets/styles/layout/landing/layout.scss';
+</style>
+
+<script>
+import Navbar from '@/components/layout/landing/Navbar.vue';
+import Footer from '@/components/layout/landing/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+
+</script>
